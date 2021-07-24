329a7c4e0899877741c4076f5957d876b4240820
Juan Galvis <jdgalviss@gmail.com>
2021-07-24 23:21:23 +0400

    feat: Local Motion Planning

--- dev_ws/src/vision/vision/navigation.py
+++ dev_ws/src/vision/vision/navigation.py
@@ -6,12 +6,21 @@ import math
 import numpy as np
 
 OSCILLATIONS_DETECTION_LENGTH = 3
+LOOK_AHEAD = 40
+Kp = 0.1
+Ki = 0.0001
+Kd = 0.0001
+HEADING_THRESHOLD = 50*math.pi/180.0
+MAX_SPEED = 0.22
 
 class NavigationSystem(object):
     def __init__(self, log = False): 
         self.perception_ = PerceptionSystem()
         self.costmap_ = CostMap(self.perception_.M_, log)
         self.planner_ = PotentialFieldPlanner(self.perception_.M_inv_)
+        # Local Control Variables
+        self.heading_error_int_ = 0.0
+        self.prev_heading_error = 0.0
 
     def transform_path(self, path, robot_state):
         yaw = robot_state[2] - math.pi/2.0
@@ -23,7 +32,7 @@ class NavigationSystem(object):
         print(path.shape)
         return path
 
-    def path_planning(self, img, robot_state):
+    def global_planner_step(self, img, robot_state):
         driveable_decoded, driveable_mask, preds, driveable_mask_with_objects = self.perception_.process_frame(img)
         cost,cost_obst = self.costmap_.calculate_costmap(driveable_mask, preds, driveable_mask_with_objects)
         path, path_img = self.planner_.calculate_path(cost)
@@ -34,6 +43,29 @@ class NavigationSystem(object):
 
         result_img, result_birdview = self.planner_.draw_result(img, cost_obst, path_img, driveable_decoded)
         return path, result_img, result_birdview
+
+    def local_planner_step(self, robot_state_, global_plan):
+        distances = (robot_state_[0]-global_plan[:,0])**2 + (robot_state_[1]-global_plan[:,1])**2
+        min_idx = np.argmin(distances) + LOOK_AHEAD
+        min_idx = min(min_idx, distances.shape[0]-1)
+        local_goal = global_plan[min_idx]
+
+        # Pure pursuit local navigation
+        pose_error = local_goal - robot_state_[:2]
+        heading_goal = math.atan2(pose_error[1],pose_error[0])
+        heading_error = heading_goal - robot_state_[2]
+        self.heading_error_int_ += heading_error
+        yaw_rate_cmd = Kp*heading_error + Ki*self.heading_error_int_ + Kd*(heading_error-self.prev_heading_error)
+        self.prev_heading_error = heading_error
+        if(abs(heading_error)<HEADING_THRESHOLD):
+            vel_cmd = 1.0*distances[min_idx]
+            if (vel_cmd > MAX_SPEED): vel_cmd = MAX_SPEED 
+        else:
+            vel_cmd = 0.0
+        print('vel_cmd: {}'.format(vel_cmd))
+        print('yaw_rate_cmd: {}'.format(yaw_rate_cmd))
+        return vel_cmd, yaw_rate_cmd
+        
 
 #     def seg2scan(self, driveable_area):
 #         h,w,_ = driveable_area.shape
--- dev_ws/src/vision/vision/vision_node.py
+++ dev_ws/src/vision/vision/vision_node.py
@@ -5,6 +5,8 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
+
 
 
 from cv_bridge import CvBridge, CvBridgeError
@@ -18,35 +20,103 @@ import math
 import time
 
 pkg_sem_seg = get_package_share_directory('vision')
-
+GLOBAL_PLAN_TIME = 1.0
+LOCAL_PLAN_TIME = 0.1
 
 class VisionNode(Node):
     def __init__(self):
         super().__init__('vision')
-        # Image subscriber
-        self.image_sub_ = self.create_subscription(Image, 'camera/image_raw', self.image_raw_callback, 1)
-        self.odom_sub_ = self.create_subscription(Odometry, 'odom', self.odom_callback, 1)
-
-        # Image publisher
+        # Publishers
         self.path_img_publisher_ = self.create_publisher(Image, 'vision/path_img', 1)
         self.birdeye_publisher_ = self.create_publisher(Image, 'vision/birdeye_img', 1)
         self.scan_publisher_ = self.create_publisher(LaserScan, 'segmentation/scan', 1)
         self.global_path_publisher_ = self.create_publisher(Path, 'global_plan', 1)
-
-        self.bridge = CvBridge() # Bridge between opencv and img msgs
-
+        self.cmd_publisher_ = self.create_publisher(Twist, 'cmd_vel', 1)
+        cmd = Twist()
+        self.cmd_publisher_.publish(cmd)
         # Create Navigation System
         self.nav_ = NavigationSystem(False)
 
         # Robot State
         self.robot_state_ = np.array([0.0,0.0,0.0])
 
+        self.bridge = CvBridge() # Bridge between opencv and img msgs
+
+        # Subscribers
+        self.image_sub_ = self.create_subscription(Image, 'camera/image_raw', self.image_raw_callback, 1)
+        self.odom_sub_ = self.create_subscription(Odometry, 'odom', self.odom_callback, 1)
+
+        
+
+
+        # Planner timers
+        self.global_plan_timer_ = self.create_timer(GLOBAL_PLAN_TIME, self.global_plan_callback)
+        self.local_plan_timer_ = self.create_timer(LOCAL_PLAN_TIME, self.local_plan_callback)
+
         self.is_saved = False
+        self.image_msg_ = None
+        self.global_plan_ = None
         print('Vision Module ready!')
 
 
+    def global_plan_callback(self):
+        if self.image_msg_ is not None:
+            image = self.bridge.imgmsg_to_cv2(self.image_msg_, desired_encoding='passthrough')
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # Save a sample image
+            if not self.is_saved:
+                cv2.imwrite("/usr/src/app/dev_ws/src/vision/dolly.png", image)
+                self.is_saved = True
+            # image = cv2.resize(image,(960,720))
+
+            # Perform one navigation step (perception and global path planning)
+            self.global_plan_, result_img, result_birdview = self.nav_.global_planner_step(image, self.robot_state_)
+            # publish topics
+            path_msg = Path()
+            path_msg.header.frame_id = 'odom_demo'
+            path_msg.header.stamp = self.image_msg_.header.stamp#self.get_clock().now().to_msg()
+            poses = []
+            for point in self.global_plan_:
+                pose = PoseStamped()
+                pose.header.frame_id = 'odom_demo'
+                pose.header.stamp = self.image_msg_.header.stamp
+                pose.pose.position.x = point[0]
+                pose.pose.position.y = point[1]
+                pose.pose.orientation.w = 1.0
+                poses.append(pose)
+            path_msg.poses = poses
+            self.global_path_publisher_.publish(path_msg)
+
+            path_img = Image()
+            path_img = self.bridge.cv2_to_imgmsg(result_img)
+            path_img.header.frame_id = 'chassis'
+            path_img.header.stamp = self.get_clock().now().to_msg()
+            path_img.encoding = self.image_msg_.encoding
+            self.path_img_publisher_.publish(path_img)
+
+            path_img = Image()
+            path_img = self.bridge.cv2_to_imgmsg(result_birdview)
+            path_img.header.frame_id = 'chassis'
+            path_img.header.stamp = self.get_clock().now().to_msg()
+            path_img.encoding = self.image_msg_.encoding
+            self.birdeye_publisher_.publish(path_img)
+
+    def local_plan_callback(self):
+        if self.global_plan_ is not None:
+            # Find point in global plan closest to robot
+            vel_cmd,yaw_rate_cmd = self.nav_.local_planner_step(self.robot_state_, self.global_plan_)
+            cmd_msg = Twist()
+            cmd_msg.linear.x = vel_cmd 
+            cmd_msg.angular.z = yaw_rate_cmd 
+            self.cmd_publisher_.publish(cmd_msg)
+        else:
+            print("No global plan")
+
+
+
+
     def odom_callback(self, msg):
-        print("Odometry received!")
+        # print("Odometry received!")
 
         # Obtain, x,y and yaw:
         x = msg.pose.pose.position.x
@@ -57,68 +127,10 @@ class VisionNode(Node):
         self.robot_state_ = np.array([x,y,yaw])
         # print(self.robot_state_)
 
-        
-
     def image_raw_callback(self, msg):
         # print("New Image received!")
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
-        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-        # Save a sample image
-        if not self.is_saved:
-            cv2.imwrite("/usr/src/app/dev_ws/src/vision/dolly.png", cv_image)
-            self.is_saved = True
-        # cv_image = cv2.resize(cv_image,(960,720))
+        self.image_msg_ = msg
 
-        # Perform one navigation step (perception and path planning)
-        path, result_img, result_birdview = self.nav_.path_planning(cv_image, self.robot_state_)
-
-        # publish topics
-        path_msg = Path()
-        path_msg.header.frame_id = 'odom_demo'
-        path_msg.header.stamp = msg.header.stamp#self.get_clock().now().to_msg()
-        poses = []
-        for point in path:
-            pose = PoseStamped()
-            pose.header.frame_id = 'odom_demo'
-            pose.header.stamp = msg.header.stamp
-            pose.pose.position.x = point[0]
-            pose.pose.position.y = point[1]
-            pose.pose.orientation.w = 1.0
-            poses.append(pose)
-        path_msg.poses = poses
-        self.global_path_publisher_.publish(path_msg)
-
-
-
-        path_img = Image()
-        path_img = self.bridge.cv2_to_imgmsg(result_img)
-        path_img.header.frame_id = 'chassis'
-        path_img.header.stamp = self.get_clock().now().to_msg()
-        path_img.encoding = msg.encoding
-        self.path_img_publisher_.publish(path_img)
-
-        path_img = Image()
-        path_img = self.bridge.cv2_to_imgmsg(result_birdview)
-        path_img.header.frame_id = 'chassis'
-        path_img.header.stamp = self.get_clock().now().to_msg()
-        path_img.encoding = msg.encoding
-        self.birdeye_publisher_.publish(path_img)
-
-        
-
-
-        # scan_msg = LaserScan()
-        # scan_msg.ranges = scan_distances
-        # scan_msg.intensities = [0.0]*len(scan_distances)
-        # scan_msg.header.stamp = self.get_clock().now().to_msg()
-        # scan_msg.header.frame_id = 'chassis'
-        # scan_msg.angle_increment = angle_increment
-        # scan_msg.angle_max = 50*math.pi/180.0
-        # scan_msg.angle_min = -50*math.pi/180.0
-        # scan_msg.range_min = 1.0
-        # scan_msg.range_max = 20.0
-        # if(len(scan_distances) > 50):
-        #     self.scan_publisher_.publish(scan_msg)
 
 def main(args = None):
     print('Hi from vision.')
