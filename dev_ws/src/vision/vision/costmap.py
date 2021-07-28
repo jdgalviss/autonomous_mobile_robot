fix: multiple bug fixes with costmap calculation

dev_ws/src/vision/vision/costmap.py
@@ -3,9 +3,11 @@ import math
 import cv2
 import matplotlib.pyplot as plt
 
-HORIZ_ANGLE_THRESHOLD = 15*math.pi/180.0
-FORWARD_WEIGHT = 2.0
-CENTER_WEIGHT = 0.4
+HORIZ_ANGLE_THRESHOLD = 20*math.pi/180.0
+FORWARD_WEIGHT = 1.5
+CENTER_WEIGHT = 0.3
+OBSTACLE_WEIGHT = 3.0#2.0
+
 HEIGHT=480
 WIDTH=480
 PIXEL_PER_METER_X = (WIDTH - 2*150)/3.0 #Horizontal distance between src points in the real world ( I assumed 3.0 meters)
@@ -22,15 +24,15 @@ class CostMap(object):
         self.log = log
         
     def calculate_costmap(self,driveable_mask, preds, driveable_mask_with_objects):
-        
-        
         # Find sidewalk edge lines and angle
         # h,w = driveable_mask.shape
         if(self.log):
             fig, ax = plt.subplots(figsize=(20, 10))
             ax.imshow(driveable_mask)
             plt.show()
+
         angle_avg, m_avg, b_avg = self.sidewalk_lines(driveable_mask, driveable_mask_with_objects) #driveable_mask_with_objects contains all objects and lines
+        
         if(self.log):
             fig, ax = plt.subplots(figsize=(20, 10))
             ax.imshow(driveable_mask_with_objects)
@@ -46,7 +48,7 @@ class CostMap(object):
         cost_forward = self.forward_cost(angle_avg)
 
         # Total cost
-        cost_fcn = cost_obst+cost_forward*FORWARD_WEIGHT+cost_center*CENTER_WEIGHT
+        cost_fcn = cost_obst*OBSTACLE_WEIGHT+cost_forward*FORWARD_WEIGHT+cost_center*CENTER_WEIGHT
 
         if(self.log):
             fig, ax = plt.subplots(figsize=(20, 10))
@@ -76,14 +78,19 @@ class CostMap(object):
             ax.set_title('Surface plot')
             ax.view_init(40, -70)
             plt.show()
-        return cost_fcn, cost_obst
+
+        display_cost = (cost_obst*OBSTACLE_WEIGHT + CENTER_WEIGHT*cost_center)
+        display_cost = display_cost / np.amax(display_cost)
+        return cost_fcn, display_cost, driveable_mask_with_objects
+        
+        # return cost_fcn, cost_obst, driveable_mask_with_objects
         
     def gaus2d(self, x=0, y=0, mx=0, my=0, sx=1, sy=1):
         return 1. / (2. * np.pi * sx * sy) * np.exp(-((x - mx)**2. / (2. * sx**2.) + (y - my)**2. / (2. * sy**2.)))
     
-    def obstacle_cost(self, mask_with_objects, gaussian_shape = 150):
+    def obstacle_cost(self, mask_with_objects, gaussian_shape = 125):
         x = np.linspace(-2, 2,gaussian_shape)
-        y = np.linspace(-2, 2,gaussian_shape*2)
+        y = np.linspace(-2, 2,int(gaussian_shape*2.5))
         x, y = np.meshgrid(x, y) # get 2D variables instead of 1D
         z = self.gaus2d(x, y)
         z = np.float32(z)
@@ -115,12 +122,13 @@ class CostMap(object):
         mask = np.uint8(mask)
         
         # Detect lines in driveable area mask
-        lines = cv2.HoughLinesP(mask, 1, 1*np.pi / 180, 100, None, 150, 50)
+        lines = cv2.HoughLinesP(mask, 1, 1*np.pi / 180, 100, None, 200, 70)
 
         line_angles = []
         lines_left = []
         lines_right = []
         lines_found = True
+        angles_horizontal = []
         if(lines is not None):
             for line in lines:
                 x2,y2,x1,y1 = line[0]
@@ -135,14 +143,25 @@ class CostMap(object):
                     x2 = x1_aux
                     y2 = y1_aux
                     angle = -math.atan2(y1-y2,x1-x2)-math.pi/2
+                angle = math.atan2(math.sin(angle), math.cos(angle))
                 line_angles.append(angle)
                     
                 # Detect horizontal lines corresponding to the corners
-                if(abs(angle-math.pi/2) < HORIZ_ANGLE_THRESHOLD):
+                if((abs(math.atan2(math.sin(angle-math.pi/2), math.cos(angle-math.pi/2))) < HORIZ_ANGLE_THRESHOLD)):
+                    # angle = -angle
+                    angles_horizontal.append(angle)
                     cv2.line(mask_out,(round(x1),round(y1)),(round(x2),round(y2)),100,1)
-                # cv2.line(mask_out,(round(x1),round(y1)),(round(x2),round(y2)),100,1)
-                
+                    is_horizontal = True
 
+                elif((abs(math.atan2(math.sin(angle+math.pi/2), math.cos(angle+math.pi/2))) < HORIZ_ANGLE_THRESHOLD)):
+                    angles_horizontal.append(angle)
+                    cv2.line(mask_out,(round(x1),round(y1)),(round(x2),round(y2)),100,1)
+                    is_horizontal = True
+                # cv2.line(mask_out,(round(x1),round(y1)),(round(x2),round(y2)),100,1)
+                else:
+                    is_horizontal = False
+                
+            
                 # Detect coordinate at the bottom of image
                 if(x2 == x1): x2 += 1 # Avoid dividing by 0
                 if(y2 == y1): y2 += 1 # Avoid dividing by 0
@@ -157,7 +176,7 @@ class CostMap(object):
                 x3 = round(-b/m)
 
                 # Add lines to the left and right list
-                if(x1<WIDTH/2):
+                if(x1<WIDTH/2 and not is_horizontal): # Condition valid when the robot is going counter clockwise
                     lines_left.append([x1,y1,x2,y2,x3,y3])
                     # lines_left.append([m,b,x2,y2,x1,y1])
                 else:
@@ -252,13 +271,36 @@ class CostMap(object):
             angle_left = -math.atan2(y1_left-y2_left,x1_left-x2_left)-math.pi/2
             angle_right = -math.atan2(y1_right-y2_right,x1_right-x2_right)-math.pi/2
 
+            angle_left = math.atan2(math.sin(angle_left), math.cos(angle_left))
+            angle_right = math.atan2(math.sin(angle_right), math.cos(angle_right))
+
+
+
             cv2.line(mask_out,(round(x1_left),round(y1_left)),(round(x2_left),round(y2_left)),255,8)
             cv2.line(mask_out,(round(x1_right),round(y1_right)),(round(x2_right),round(y2_right)),255,8)
             # Calculate middle line
-            angle_avg = (angle_left + angle_right) / 2.0
+            if len(angles_horizontal) > 0:
+                # angle_horizontal = np.average(np.array(angles_horizontal))
+                # angle_avg = (angle_left + angle_right + 4.0*angle_horizontal) / 6.0
+                angle_avg = (angle_left + angle_right) / 2.0
+
+                if angle_avg < 0.0:
+                    angle_avg += math.pi
+                # print(angle_horizontal)
+
+            else:
+                angle_avg = (angle_left + angle_right) / 2.0
+            angle_avg = math.atan2(math.sin(angle_avg), math.cos(angle_avg))
+            if((angle_avg>math.pi/2 and angle_avg<math.pi) or (angle_avg>-math.pi and angle_avg<-math.pi/2)):
+                angle_avg += math.pi
+            angle_avg = math.atan2(math.sin(angle_avg), math.cos(angle_avg))
+            
+
+            angle_avg = max(min(angle_avg, math.pi/4.0), -math.pi/4.0)
             m_avg = math.tan(-angle_avg+math.pi/2.0)
             x_center = ( x1_left + x1_right ) / 2.0
             b_avg = HEIGHT - m_avg*x_center
+
         else:
             m_avg = None
             b_avg = None

dev_ws/src/vision/vision/dl_perception.py
@@ -48,13 +48,13 @@ class PerceptionSystem(object):
                 x = w_rate*(pred[0]+pred[2])/2.0 # Ground middle point
                 y = h_rate*pred[3]
                 if(pred[5]==0): #person
-                    wr = 20
-                    hr = 20
-                    color = 150
+                    wr = 40
+                    hr = 60
+                    color = 255
                 else:
-                    wr = 20
-                    hr = 40
-                    color = 150
+                    wr = 30
+                    hr = 90
+                    color = 255
                 pos_orig = np.array([[[x,y]]],dtype=np.float32)
                 warped_birdview = cv2.perspectiveTransform(pos_orig, self.M_)[0][0] # Transform middle ground point to birdview
                 warped_birdview = np.uint16(warped_birdview)
@@ -76,4 +76,4 @@ class PerceptionSystem(object):
         driveable_mask_with_objects = driveable_mask.copy()
         self.add_detections_birdview(preds, driveable_mask_with_objects)
         
-        return driveable_decoded, driveable_mask, preds, driveable_mask_with_objects
+        return driveable_decoded, driveable_mask, preds, driveable_mask_with_objects, img_decoded

dev_ws/src/vision/vision/navigation.py
@@ -6,12 +6,12 @@ import math
 import numpy as np
 
 OSCILLATIONS_DETECTION_LENGTH = 3
-LOOK_AHEAD = 40
-Kp = 0.1
-Ki = 0.0001
-Kd = 0.0001
-HEADING_THRESHOLD = 50*math.pi/180.0
-MAX_SPEED = 0.22
+LOOK_AHEAD = 45
+Kp = 0.22
+Ki = 0.001
+Kd = 0.0
+HEADING_THRESHOLD = 45*math.pi/180.0
+MAX_SPEED = 0.45
 
 class NavigationSystem(object):
     def __init__(self, log = False): 
@@ -29,12 +29,11 @@ class NavigationSystem(object):
         path = np.array([path_x,path_y])
         path = np.transpose(path)
         path += robot_state[:2]
-        print(path.shape)
         return path
 
     def global_planner_step(self, img, robot_state):
-        driveable_decoded, driveable_mask, preds, driveable_mask_with_objects = self.perception_.process_frame(img)
-        cost,cost_obst = self.costmap_.calculate_costmap(driveable_mask, preds, driveable_mask_with_objects)
+        driveable_decoded, driveable_mask, preds, driveable_mask_with_objects, img_decoded = self.perception_.process_frame(img)
+        cost,cost_obst, driveable_mask = self.costmap_.calculate_costmap(driveable_mask, preds, driveable_mask_with_objects)
         path, path_img = self.planner_.calculate_path(cost)
 
         # Transform path to global frame
@@ -42,7 +41,9 @@ class NavigationSystem(object):
             path = self.transform_path(path, robot_state)
 
         result_img, result_birdview = self.planner_.draw_result(img, cost_obst, path_img, driveable_decoded)
-        return path, result_img, result_birdview
+        return path, driveable_mask, result_birdview
+
+        # return path, result_img, result_birdview
 
     def local_planner_step(self, robot_state_, global_plan):
         distances = (robot_state_[0]-global_plan[:,0])**2 + (robot_state_[1]-global_plan[:,1])**2
@@ -54,16 +55,19 @@ class NavigationSystem(object):
         pose_error = local_goal - robot_state_[:2]
         heading_goal = math.atan2(pose_error[1],pose_error[0])
         heading_error = heading_goal - robot_state_[2]
+
+        heading_error = math.atan2(math.sin(heading_error),math.cos(heading_error))
+
         self.heading_error_int_ += heading_error
-        yaw_rate_cmd = Kp*heading_error + Ki*self.heading_error_int_ + Kd*(heading_error-self.prev_heading_error)
+        yaw_rate_cmd = Kp*(heading_error + Ki*self.heading_error_int_ + Kd*(heading_error-self.prev_heading_error))
         self.prev_heading_error = heading_error
         if(abs(heading_error)<HEADING_THRESHOLD):
             vel_cmd = 1.0*distances[min_idx]
             if (vel_cmd > MAX_SPEED): vel_cmd = MAX_SPEED 
         else:
             vel_cmd = 0.0
-        print('vel_cmd: {}'.format(vel_cmd))
-        print('yaw_rate_cmd: {}'.format(yaw_rate_cmd))
+        # print('vel_cmd: {}'.format(vel_cmd))
+        # print('yaw_rate_cmd: {}'.format(yaw_rate_cmd))
         return vel_cmd, yaw_rate_cmd
         
 

dev_ws/src/vision/vision/planner.py
@@ -13,31 +13,52 @@ class PotentialFieldPlanner(object):
                           
     def __init__(self, M_inv):
         self.M_inv_ = M_inv
+        self.motion_model = self.get_motion_model(5)
         print("Planner")
                          
-    def get_motion_model(self):
+    def get_motion_model(self, size):
         # dx, dy
-        motion = [
-                #   [2, 0], #right
-    #               [0, 2], #back
-                #   [-2, 0], #left
-                  #[0, -3], # front
-                  [-1, -2],#front-left
-                  [-2, -2],#front-left
-                  [-2, -1],#front-left
+        motion_model = []
+        for i in range(-size,size):
+            for j in range(1,size):
+                if(i != 0):
+                    motion_model.append([i,-j])
 
-                    #   [-3, -4],#front-left
-                    #   [-4, -4],#front-left
-    #               [-1, 2], # back-left
-                  [1, -2], #front-right
-                  [2, -2], #front-right
-                  [2, -1], #front-right
+    #     self.motion_model = [
+    #             #   [2, 0], #right
+    # #               [0, 2], #back
+    #             #   [-2, 0], #left
+    #               #[0, -3], # front
+    #               [-1, -2],#front-left
+    #               [-2, -2],#front-left
+    #               [-2, -1],#front-left
 
-                #   [3, -4], #front-right
-                #   [4, -4], #front-right
-    # #               [1, 1]  #back-right
-                  ]
-        return motion
+    #               [-1, -4],#front-left
+    #               [-3, -4],#front-left
+
+    #               [-3, -1],#front-left
+    #               [-3, -2],#front-left
+
+
+
+    #                 #   [-3, -4],#front-left
+    #                 #   [-4, -4],#front-left
+    # #               [-1, 2], # back-left
+    #               [1, -2], #front-right
+    #               [2, -2], #front-right
+    #               [2, -1], #front-right
+
+    #               [1, -4],#front-right
+    #               [3, -4],#front-right
+
+    #               [3, -1],#front-right
+    #               [3, -2],#front-right
+
+    #             #   [3, -4], #front-right
+    #             #   [4, -4], #front-right
+    # # #               [1, 1]  #back-right
+                #   ]
+        return motion_model
 
     def oscillations_detection(self,previous_ids, ix, iy):
         previous_ids.append((ix, iy))
@@ -54,22 +75,22 @@ class PotentialFieldPlanner(object):
 
     def calculate_path(self,pmap):
         output = pmap.copy()*0
-        motion = self.get_motion_model()
+        # self.motion_model = self.get_motion_model()
         previous_ids = deque()
         ix = round(WIDTH/2)
-        iy = round(HEIGHT) 
+        iy = round(HEIGHT-5) 
         path = []
         while(ix > 5 and iy > 50 and ix< WIDTH-5):
             minp = float("inf")
             minix, miniy = 0, -1
-            for i, _ in enumerate(motion):
-                inx = int(ix + 1*motion[i][0])
-                iny = int(iy + 1*motion[i][1])
+            for i, _ in enumerate(self.motion_model):
+                inx = int(ix + 1*self.motion_model[i][0])
+                iny = int(iy + 1*self.motion_model[i][1])
                 if inx >= WIDTH or iny >= HEIGHT or inx < 0 or iny < 0:
                     p = float("inf")  # outside area
                     print("outside potential!")
                 else:
-                    p = pmap[iny][inx] * (math.sqrt(motion[i][0]**2 + motion[i][1]**2))
+                    p = pmap[iny][inx] * (math.sqrt(self.motion_model[i][0]**2 + self.motion_model[i][1]**2))
 
                 if minp >= p:
                     minp = p
@@ -94,7 +115,7 @@ class PotentialFieldPlanner(object):
         result_birdview = np.uint8(result_birdview*255.0)
         unwarped_birdview = cv2.warpPerspective(result_birdview, self.M_inv_, (w_orig,h_orig), flags=cv2.INTER_LINEAR)
     #     unwarped_birdview = np.uint8(unwarped_birdview*255.0)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         output = cv2.addWeighted(img, 0.8, unwarped_birdview, 0.5, 0)  
         output = cv2.addWeighted(output, 0.9, driveable_mask, 0.1, 0)    
 

dev_ws/src/vision/vision/vision_node.py
@@ -5,9 +5,9 @@ from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Twist
-
-
+from nav_msgs.msg import OccupancyGrid
 
 from cv_bridge import CvBridge, CvBridgeError
 from ament_index_python.packages import get_package_share_directory
@@ -20,8 +20,13 @@ import math
 import time
 
 pkg_sem_seg = get_package_share_directory('vision')
-GLOBAL_PLAN_TIME = 1.0
-LOCAL_PLAN_TIME = 0.1
+GLOBAL_PLAN_TIME = 0.5
+LOCAL_PLAN_TIME = 0.05
+
+HEIGHT=480
+WIDTH=480
+PIXEL_PER_METER_X = (WIDTH - 2*150)/3.0 #Horizontal distance between src points in the real world ( I assumed 3.0 meters)
+PIXEL_PER_METER_Y = (HEIGHT - 30-60)/8.0 #Vertical distance between src points in the real world ( I assumed 6.0 meters)
 
 class VisionNode(Node):
     def __init__(self):
@@ -32,6 +37,8 @@ class VisionNode(Node):
         self.scan_publisher_ = self.create_publisher(LaserScan, 'segmentation/scan', 1)
         self.global_path_publisher_ = self.create_publisher(Path, 'global_plan', 1)
         self.cmd_publisher_ = self.create_publisher(Twist, 'cmd_vel', 1)
+        self.costmap_publisher_ = self.create_publisher(OccupancyGrid, 'costmap', 1)
+
         cmd = Twist()
         self.cmd_publisher_.publish(cmd)
         # Create Navigation System
@@ -46,9 +53,6 @@ class VisionNode(Node):
         self.image_sub_ = self.create_subscription(Image, 'camera/image_raw', self.image_raw_callback, 1)
         self.odom_sub_ = self.create_subscription(Odometry, 'odom', self.odom_callback, 1)
 
-        
-
-
         # Planner timers
         self.global_plan_timer_ = self.create_timer(GLOBAL_PLAN_TIME, self.global_plan_callback)
         self.local_plan_timer_ = self.create_timer(LOCAL_PLAN_TIME, self.local_plan_callback)
@@ -62,7 +66,7 @@ class VisionNode(Node):
     def global_plan_callback(self):
         if self.image_msg_ is not None:
             image = self.bridge.imgmsg_to_cv2(self.image_msg_, desired_encoding='passthrough')
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             # Save a sample image
             if not self.is_saved:
                 cv2.imwrite("/usr/src/app/dev_ws/src/vision/dolly.png", image)
@@ -70,36 +74,67 @@ class VisionNode(Node):
             # image = cv2.resize(image,(960,720))
 
             # Perform one navigation step (perception and global path planning)
-            self.global_plan_, result_img, result_birdview = self.nav_.global_planner_step(image, self.robot_state_)
-            # publish topics
-            path_msg = Path()
-            path_msg.header.frame_id = 'odom_demo'
-            path_msg.header.stamp = self.image_msg_.header.stamp#self.get_clock().now().to_msg()
-            poses = []
-            for point in self.global_plan_:
-                pose = PoseStamped()
-                pose.header.frame_id = 'odom_demo'
-                pose.header.stamp = self.image_msg_.header.stamp
-                pose.pose.position.x = point[0]
-                pose.pose.position.y = point[1]
-                pose.pose.orientation.w = 1.0
-                poses.append(pose)
-            path_msg.poses = poses
-            self.global_path_publisher_.publish(path_msg)
+            global_plan, result_img, result_birdview = self.nav_.global_planner_step(image, self.robot_state_)
+            if(global_plan.shape[0] > 1):
+                self.global_plan_ = global_plan
+                # Publish topics
+                #Path
+                path_msg = Path()
+                path_msg.header.frame_id = 'odom_demo'
+                path_msg.header.stamp = self.image_msg_.header.stamp#self.get_clock().now().to_msg()
+                poses = []
+                for point in self.global_plan_:
+                    pose = PoseStamped()
+                    pose.header.frame_id = 'odom_demo'
+                    pose.header.stamp = self.image_msg_.header.stamp
+                    pose.pose.position.x = point[0]
+                    pose.pose.position.y = point[1]
+                    pose.pose.orientation.w = 1.0
+                    poses.append(pose)
+                path_msg.poses = poses
+                self.global_path_publisher_.publish(path_msg)
 
-            path_img = Image()
-            path_img = self.bridge.cv2_to_imgmsg(result_img)
-            path_img.header.frame_id = 'chassis'
-            path_img.header.stamp = self.get_clock().now().to_msg()
-            path_img.encoding = self.image_msg_.encoding
-            self.path_img_publisher_.publish(path_img)
+                # Costmap
+                costmap_msg = OccupancyGrid()
+                costmap_msg.header.frame_id = 'odom_demo'
+                costmap_msg.header.stamp = self.image_msg_.header.stamp
+                costmap_msg.info.resolution = 4.0/PIXEL_PER_METER_X
+                costmap = cv2.resize(result_birdview[:,:,0], (int(WIDTH/4.0),int((HEIGHT*PIXEL_PER_METER_X/PIXEL_PER_METER_Y)/4.0)))
+                costmap = cv2.flip(costmap,-1)
+                costmap_msg.info.width = costmap.shape[0]
+                costmap_msg.info.height = costmap.shape[1]
+                origin_costmap = poses[0].pose
+                origin_costmap.orientation = self.robot_orientation_
+                origin_costmap.position.y -= (WIDTH/(PIXEL_PER_METER_X*2.0))*math.cos(self.robot_state_[2])# Half of the image
+                origin_costmap.position.x += (WIDTH/(PIXEL_PER_METER_X*2.0))*math.sin(self.robot_state_[2])# Half of the image
+                # origin_costmap.orientation.w = 1.0
+                costmap_msg.info.origin = origin_costmap
+                costmap = np.transpose(costmap)
+                costmap = costmap.flatten()
+                costmap = np.int8(100.0*np.float32(costmap)/255.0)
+                costmap_msg.data = costmap.tolist()
+                self.costmap_publisher_.publish(costmap_msg)
 
-            path_img = Image()
-            path_img = self.bridge.cv2_to_imgmsg(result_birdview)
-            path_img.header.frame_id = 'chassis'
-            path_img.header.stamp = self.get_clock().now().to_msg()
-            path_img.encoding = self.image_msg_.encoding
-            self.birdeye_publisher_.publish(path_img)
+                path_img = Image()
+                
+
+                result_img = cv2.cvtColor(np.uint8(result_img), cv2.COLOR_GRAY2RGB)
+
+                path_img = self.bridge.cv2_to_imgmsg(result_img)
+                path_img.header.frame_id = 'chassis'
+                path_img.header.stamp = self.get_clock().now().to_msg()
+                path_img.encoding = self.image_msg_.encoding
+                self.path_img_publisher_.publish(path_img)
+
+                path_img = Image()
+                path_img = self.bridge.cv2_to_imgmsg(result_birdview)
+
+                path_img.header.frame_id = 'chassis'
+                path_img.header.stamp = self.get_clock().now().to_msg()
+                path_img.encoding = self.image_msg_.encoding
+                self.birdeye_publisher_.publish(path_img)
+            else:
+                print("No global plan generated")
 
     def local_plan_callback(self):
         if self.global_plan_ is not None:
@@ -121,6 +156,7 @@ class VisionNode(Node):
         # Obtain, x,y and yaw:
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
+        self.robot_orientation_ = msg.pose.pose.orientation
 
         _,_,yaw = euler_from_quaternion(msg.pose.pose.orientation)
 